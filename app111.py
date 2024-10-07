ac462f5d146f4be5537ef021681755868beaa83d
brodyhimself24 <brodyhzt17@sina.com>
2024-10-08 05:24:44 +0800

    Update app111.py

--- app111.py
+++ app111.py
@@ -1,8 +1,14 @@
 import numpy as np
+import pandas as pd
 import streamlit as st
 from joblib import load
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-# 设置matplotlib字体，以便支持中文和负号
+import matplotlib.pyplot as plt  # 导入 matplotlib.pyplot
+
+# 设置 matplotlib 配置，以便支持中文和负号
+
 plt.rcParams['font.sans-serif'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] = False
 
