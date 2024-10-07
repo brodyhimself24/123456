bafee32c1b450f9bbf346563e9bfeba46f6ad8c9
brodyhimself24 <brodyhzt17@sina.com>
2024-10-08 04:52:46 +0800

    Add files via upload

--- app111.py
+++ app111.py
@@ -10,8 +10,6 @@ def main():
 
     # 创建文本输入框供用户输入特征数据
     # ...（省略了输入框的代码以节省空间）...
-
-# 创建文本输入框供用户输入特征数据
     weight = st.number_input("请输入体重 (kg)", min_value=0, max_value=150, value=65)
     height = st.number_input("请输入身高 (cm)", min_value=0, max_value=250, value=175)
     age = st.number_input("请输入年龄", min_value=0, max_value=120, value=25)
@@ -30,18 +28,20 @@ def main():
     tr250 = st.number_input("请输入250Hz的TR值", min_value=0, max_value=500, value=50)
     rl250 = st.number_input("请输入250Hz的RL值", min_value=0, max_value=500, value=120)
     ll250 = st.number_input("请输入250Hz的LL值", min_value=0, max_value=500, value=120)
-
     # 用户点击预测按钮
     if st.button('进行预测'):
-        # 加载模型和标准化器
-        model = load('heu11.joblib')
-        scaler_X = load('scaler_X11.joblib')
-        poly = PolynomialFeatures(degree=2, include_bias=False)  # 确保与训练时的多项式度数一致
+        # 加载模型、标准化器和多项式特征生成器
+        model = load('model.joblib')  # 确保这是正确的文件名
+        scaler_X = load('scaler_X.joblib')  # 确保这是正确的文件名
+        poly = load('poly.joblib')  # 加载多项式特征生成器
 
         # 构建特征数组
-        features = np.array([weight, height, age, ra5, la5, tr5, rl5, ll5, ra50, la50, tr50, rl50, ll50, ra250, la250, tr250, rl250, ll250]).reshape(1, -1)
+        features = np.array([
+            weight, height, age, ra5, la5, tr5, rl5, ll5, ra50, la50, tr50, rl50, ll50, 
+            ra250, la250, tr250, rl250, ll250
+        ]).reshape(1, -1)
 
-        # 生成多项式和交互特征
+        # 使用加载的 PolynomialFeatures 实例生成多项式和交互特征
         features_poly = poly.transform(features)
 
         # 使用加载的 StandardScaler 实例进行特征标准化
@@ -52,7 +52,11 @@ def main():
 
         # 显示预测结果
         st.write("预测结果如下：")
-        labels = ['体脂率1', '肌肉1', '骨骼肌1', '水分量1', '蛋白质1', '无机盐1', '内脏脂肪1', '基础代谢1', '节段肌肉左臂1', '节段肌肉右臂1', '节段肌肉躯干1', '节段肌肉左腿1', '节段肌肉右腿1', '骨量1', '腰臀比1', '浮肿评估1']
+        labels = [
+            '体脂率1', '肌肉1', '骨骼肌1', '水分量1', '蛋白质1', '无机盐1', '内脏脂肪1', 
+            '基础代谢1', '节段肌肉左臂1', '节段肌肉右臂1', '节段肌肉躯干1', '节段肌肉左腿1', 
+            '节段肌肉右腿1', '骨量1', '腰臀比1', '浮肿评估1'
+        ]
         if len(prediction) > 0:
             for i, label in enumerate(labels):
                 st.write(f"{label}: {prediction[0][i]:.2f}")
