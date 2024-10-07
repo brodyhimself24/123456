b4973ec7819e1b01b6c04e69ff2445d7bf2fabfb
brodyhimself24 <brodyhzt17@sina.com>
2024-10-08 05:30:05 +0800

    Add files via upload

--- app111.py
+++ app111.py
@@ -5,17 +5,12 @@ from joblib import load
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt  # 导入 matplotlib.pyplot
-
-# 设置 matplotlib 配置，以便支持中文和负号
-
-plt.rcParams['font.sans-serif'] = 'SimHei'
-plt.rcParams['axes.unicode_minus'] = False
 
 # Streamlit界面
 def main():
     st.title('健康指标预测')
 
+    # 创建文本输入框供用户输入特征数据
     # 创建文本输入框供用户输入特征数据
     weight = st.number_input("请输入体重 (kg)", min_value=0, max_value=150, value=65)
     height = st.number_input("请输入身高 (cm)", min_value=0, max_value=250, value=175)
@@ -35,15 +30,17 @@ def main():
     tr250 = st.number_input("请输入250Hz的TR值", min_value=0, max_value=500, value=50)
     rl250 = st.number_input("请输入250Hz的RL值", min_value=0, max_value=500, value=120)
     ll250 = st.number_input("请输入250Hz的LL值", min_value=0, max_value=500, value=120)
-
-    # 构建特征数组
-    features = np.array([[weight, height, age, ra5, la5, tr5, rl5, ll5, ra50, la50, tr50, rl50, ll50, ra250, la250, tr250, rl250, ll250]])
-
     # 用户点击预测按钮
     if st.button('进行预测'):
         # 加载模型和标准化器
-        model = load('heu12.joblib')  # 确保这是正确的文件名
-        scaler_X = load('scaler_X12.joblib')  # 确保这是正确的文件名
+        model = load('model.joblib')  # 确保这是正确的文件名
+        scaler_X = load('scaler_X.joblib')  # 确保这是正确的文件名
+
+        # 构建特征数组，并确保是二维形状
+        features = np.array([
+            weight, height, age,ra5, la5, tr5, rl5, ll5, ra50, la50, tr50, rl50, ll50, ra250, la250, tr250, rl250, ll250
+            # ...（省略了其他特征的代码以节省空间）...
+        ]).reshape(1, -1)
 
         # 使用加载的 StandardScaler 实例进行特征标准化
         features_scaled = scaler_X.transform(features)
@@ -53,8 +50,11 @@ def main():
 
         # 显示预测结果
         st.write("预测结果如下：")
-        labels = ['体脂率', '肌肉', '骨骼肌', '水分率百分比', '蛋白质', '无机盐', '内脏脂肪', 
-                  '节段肌肉左臂', '节段肌肉右臂', '节段肌肉躯干', '节段肌肉左腿', '节段肌肉右腿', '骨盐量', '腰臀比']
+        labels = [
+            '体脂率', '肌肉', '骨骼肌', '水分率百分比', '蛋白质', '无机盐', '内脏脂肪', 
+                  '节段肌肉左臂', '节段肌肉右臂', '节段肌肉躯干', '节段肌肉左腿', '节段肌肉右腿', '骨盐量', '腰臀比'
+            # ...（省略了其他标签的代码以节省空间）...
+        ]
         if len(prediction) > 0:
             for i, label in enumerate(labels):
                 st.write(f"{label}: {prediction[0][i]:.2f}")
